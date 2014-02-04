c34b710f5136e1d0b3c3a4b6d7bf4d3f95e7c760
eirikh <eirik.hjertenaes@ntnu.no>
2014-02-04 14:54:30 +0100

    Tuesday at 15.00

---
+++ main_serial.f90
@@ -0,0 +1,37 @@
+implicit none
+
+integer(kind=8) :: i,j,n
+integer(kind=8), parameter :: sumsize = 20
+integer(kind=8), parameter :: ndim = 2**sumsize 
+real(kind=8), parameter :: sinf = (4*datan(1.0d0))**2/6.0D0
+real(kind=8):: xerror(sumsize)
+real(kind=8):: vector(ndim)
+real(kind=8):: xsum(sumsize)
+
+
+data xsum/ sumsize * 0.0D0/
+
+do i = 1,ndim
+   vector(i) = 1.0D0/(i*i)
+enddo
+
+xsum(1) = vector(1)
+
+do i = 1,sumsize
+   n=2**i
+   do j=n/2+1,n
+      xsum(i) = xsum(i) + vector(j)
+   enddo
+enddo
+
+do i = 2,sumsize
+   xsum(i) = xsum(i) + xsum(i-1)
+   xerror(i) = sinf - xsum(i)
+enddo
+
+!Output
+do i = 3,sumsize
+   write(*,*) xsum(i), xerror(i), (xsum(i) + xerror(i))
+enddo
+
+end program
