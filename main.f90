59f45bb53392a1241d6122ffbe1793bf2d51e20e
rolfheil <rolf.h.myhre@ntnu.no>
2014-02-04 13:02:36 +0100

    Changes to main

--- main.f90
+++ main.f90
@@ -1,16 +1,25 @@
 program main
 
-integer, kind(=8):: ndim
-real, kind(=8):: vector(:)
-real, kind(=8):: xsum(:)
+implicit none
 
-write(*,*) "Enter size of vector v"
-read(*,*) ndim
+integer :: i,j,n
+integer, parameter, kind(=8):: ndim = 16384
+real, kind(=8):: vector(ndim)
+real, kind(=8):: xsum(12)
+
+!write(*,*) "Enter size of vector v"
+!read(*,*) ndim
 
 do i = 1,ndim
    vector(i) = 1.0D0/(i*i)
-   xsum(i) = xsum(i) + vector(i)
+enddo
 
+do i = 3,14
+   n=2**i
+   do j=1,n
+      xsum(i) = xsum(i) + vector(j)
+   enddo
+enddo
 
 
 end program
